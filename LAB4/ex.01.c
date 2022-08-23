9f35b86ea9c9c230afb6a93c0fff848b5ebd2078
c2111295 <c2111295@PEIXE>
2022-08-23 12:35:02 -0300

    12:34

---
+++ LAB4/ex.01.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+
+void dump (void *p, int n) {
+  unsigned char *p1 = p;
+  while (n--) {
+    printf("%p - %02x\n", p1, *p1);
+    p1++;
+  }
+}
+
+int main (void) {
+  char c = 150;
+  short s = -3;
+  int i = -151;
+  printf("%d \n",sizeof(c));
+  printf("%d \n",sizeof(s));
+  printf("%d \n",sizeof(long int));
+  printf("dump de c: \n");
+  dump(&c, sizeof(c));
+  printf("dump de s: \n");
+  dump(&s, sizeof(s));
+  printf("dump de i: \n");
+  dump(&i, sizeof(i));
+  return 0;
+}
