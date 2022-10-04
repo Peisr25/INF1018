26b6ce7addf279ce5b06326445cf79d8e6a94d32
c2111295 <c2111295@PEIXE>
2022-10-04 12:57:48 -0300

    12:57

--- T1/conv_utf.c
+++ T1/conv_utf.c
@@ -1,11 +1,15 @@
 #include <stdio.h>
 
 int converteUtf8Para32(FILE *arquivo_entrada, FILE *arquivo_saida){
-    char aux;
-    while(aux = fgetc(arquivo_entrada)!= EOF){
-        putchar(aux);
-        printf("a");
-    }
+    unsigned char aux;
+    unsigned char auxili;
+    fread(&aux,sizeof(unsigned char),1,arquivo_entrada);
+    printf("%c",aux);
+    /*while(auxili = fgetc(arquivo_entrada)!= EOF){
+        //putchar(aux);
+        //printf("a");
+        printf("%c",aux);
+    }*/
 }
 int main(void){
     FILE* a = fopen("utf8_peq.txt","r");
