055cec529c714ab551a70417e753f16f8e7ec967
JoãoVictor <jvoamorim@gmail.com>
2022-10-04 20:55:50 -0300

    20:55

--- LAB7/ex3.s
+++ LAB7/ex3.s
@@ -7,44 +7,47 @@
     }
     return 0;
 }*/
-  .data
-  Sf: .string "%d/n"
-  val: .int 1,2,3,4,5,6,7,8,9,10
+.data
+Sf: .string "%d/n"
+val: .int 1,2,3,4,5,6,7,8,9,10
 
-  .text
-  .globl main
-  main:
+.text
+.globl main
+main:
   /* prologo */
-    pushq %rbp #aponta para o topo da pilha
-    movq  %rsp, %rbp
+  pushq %rbp               #aponta para o topo da pilha
+  movq  %rsp, %rbp
+  movq    %rbx, -8(%rbp)   /* guarda rbx */
+  movq    %r12, -16(%rbp)  /* guarda r12 */
 
-    /*
-    Dicionario 
-    Variavel Registrador
-    i           EBX
-    p           R12
-    temp        EAX
-    */
-    movq $val, %r12         #int val[] == %r12
-    movl $0, %ebx           #int i=0
-    movl $0, %eax
-    #i<10
+  /*
+  Dicionario 
+  Variavel Registrador
+  i           EBX
+  p           R12
+  temp        EAX
+  */
+
+  movq $val, %r12         #int val[] == %r12
+  movl $0, %ebx           #int i=0
+  movl $0, %eax           #temp = 0;  
+  #i<10
 Loop:
-    cmpl $10, %ebx          #if(i==10)
-    je FimLoop              #jmp to return
+  cmpl $10, %ebx          #if(i==10)
+  je FimLoop              #jmp to return
+  movl (%r12), %eax       #temp += val*val
+  imul %eax, %eax         #val[i]*val[i]
+  addq $4, %r12           #r12+=4
+  addl $1, %ebx           #i++
 
-    imul %r12, %r12    #val[i]*val[i]
-    movl (%r12), %eax       #temp += val*val
-    addq $4, %r12           #r12+=4
-    addl $1, %ebx           #i++
-
-    movq    $Sf, %rdi    /* primeiro parametro (ponteiro)*/
-    movl    %ebx, %esi   /* segundo parametro  (inteiro) */
-    movl  $0, %ebx
-    call  printf       /* chama a funcao da biblioteca */
+  movq  $Sf, %rdi    /* primeiro parametro (ponteiro)*/
+  movl  %eax, %esi   /* segundo parametro  (inteiro) */
+  movl  $0, %eax
+  call  printf       /* chama a funcao da biblioteca */
+  jmp Loop
 
 
 FimLoop:
-    movl $0, %eax
-    leave
-    ret
+  movl $0, %eax
+  leave
+  ret
