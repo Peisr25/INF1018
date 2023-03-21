f9f30bbe873e8300569f0bfe6a9fb7145f1d03af
Peisr25 <jvoamorim@gmail.com>
2023-03-20 22:01:44 -0300

    ex2 finalizado

--- 2023.1/LAB3/ex02.c
+++ 2023.1/LAB3/ex02.c
@@ -1,7 +1,17 @@
 #include <stdio.h>
 
 int odd_ones(unsigned int x) {
-  
+  int contOdd;
+  for(int i = 0; i < 32; i++){
+    if(x & 1){
+      contOdd++;
+    }
+    x=x >> 1; //x>>=1;
+  }
+  if((contOdd & 1) == 0){
+    return 0;
+  }
+  return  1;
 }
 int main() {
   printf("%x tem numero %s de bits\n",0x01010101,odd_ones(0x01010101) ? "impar":"par");
