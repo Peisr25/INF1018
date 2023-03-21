e0f35bb8d81cf5f0d90dc371f2432e9b4eaca3e1
Peisr25 <jvoamorim@gmail.com>
2023-03-21 17:42:57 -0300

    ex 4 feio

---
+++ 2023.1/LAB4/ex04b.c
@@ -0,0 +1,12 @@
+/* programa 2*/
+#include <stdio.h>
+
+int main (void) {
+  unsigned int x = 0xffffffff;
+  unsigned int y = 2;
+  printf("x=%d, y=%d\n", x, y);
+  printf("x é menor do que y? %s\n", (x<y)?"sim":"nao");
+  return 0;
+}
+
+//a representação interna dos numeros muda ou seja 4294967295 é maior do que 2.
