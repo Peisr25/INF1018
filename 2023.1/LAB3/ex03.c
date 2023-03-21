35dbc9766653133ba548e300166772eaca29992f
Peisr25 <jvoamorim@gmail.com>
2023-03-20 22:32:54 -0300

    ex03 A finalizado

---
+++ 2023.1/LAB3/ex03.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+
+unsigned char switch_byte(unsigned char x){
+    unsigned char part1 = (x & 0xF0) >> 4; //1111 1111
+    unsigned char part2 = (x & 0x0F) << 4;
+    return part1 | part2 // faz a troca
+    
+
+}
