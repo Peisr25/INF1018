10bc2c18d7599c23c43202bd58bdc5d1bd29a66e
Peisr25 <jvoamorim@gmail.com>
2023-05-30 21:25:44 -0300

    gera.c sem a alteração para retornar como 2 param

--- 2023.1/T2/gera.c
+++ 2023.1/T2/gera.c
@@ -1,20 +1,122 @@
-#include <stdio.h>
+#include<stdio.h>
+#include<stdlib.h>
+#include<string.h>
+
 #define MAXLIN 30
+#define MAXCOD 30
+#define MAXPARAM 30
 
-void addVarLocal(int idx, int linha)
-{
-    if (idx > MAXLIN)
-        error("variavel local nao declarada", linha);
-    codigo[i++] = 0xff >> (2 * n);
-}
-
+int i;
+unsigned char *codigo;
 typedef int (*funcp)();
 
-funcp gera(FILE *f, unsigned char codigo[])
+static void error (const char *msg, int line) {
+  fprintf(stderr, "erro %s na linha %d\n", msg, line);
+  exit(EXIT_FAILURE);
+}
+
+void addVarLocal(int n, int linha)
 {
+    if (n > MAXLIN)
+        error("variavel local nao declarada", linha);
+    codigo[i++] = 0xff - 4 * n + 1;
+}
+
+void operador(char op, char var1, char var2,int idx0,int idx1,int idx2, int linha){
+    if(var1 == '$' && var2 == '$'){
+        codigo[i++] = 0xB8;             // mov eax, idx0
+        *((int*)&codigo[i]) = idx1;
+        i += 4;
+
+        if(op == '+'){
+            codigo[i++] = 0x05;
+            *((int*)&codigo[i]) = idx2;
+            i += 4;
+        }
+        if(op == '-'){
+            codigo[i++] = 0x83;         // sub eax, idx2
+            codigo[i++] = 0xE8;         // call
+            *((int*)&codigo[i]) = idx2;
+            i += 4;
+            i++;
+        }
+        else{
+            codigo[i++] = 0x6B;
+            codigo[i++] = 0xC0;
+            *((int*)&codigo[i]) = idx2;
+            i +=1;
+        }
+    }
+    else if(var1 == 'v' && var2 == '$'){
+        codigo[i++] == 0x8B;
+        codigo[i++] == 0x45;
+        addVarLocal(idx1,linha);        // salva a variavel local vN
+        if(op == '+'){
+            codigo[i++] = 0x83;         //add 
+            codigo[i++] = 0xC0;         // sal
+        }
+        else if(op == '-'){
+            codigo[i++] = 0x83;         // mov %eax
+            codigo[i++] = 0xE8;         // call
+        }
+        else{
+            codigo[i++] = 0x6B;
+            codigo[i++] = 0xC0;
+        }
+        *((int*)&codigo[i]) = idx2;
+        i +=1;
+    }
+    else if(var1 == 'v' && var2 == 'v'){    //operacao entre duas var 'v1+v2'
+        codigo[i++] = 0x8B;
+        codigo[i++] = 0x45;
+        addVarLocal(idx1,linha);
+        if(op == '+'){
+            codigo[i++] = 0x03;
+        }
+        if(op == '-'){
+            codigo[i++] = 0x2B;
+        }
+        else{
+            codigo[i++] = 0x0F;
+            codigo[i++] = 0xAF;
+        }
+        codigo[i++] = 0x45;
+        addVarLocal(idx2,linha);
+    }
+    else{
+        error("operação invalida",linha);
+    }
+    codigo[i++] = 0x89;
+    codigo[i++] = 0x45;
+    addVarLocal(idx0,linha);
+    return;
+}
+void libera(void *vetor){
+    free(vetor);
+}
+void iniciaPilha(unsigned char * codigo){
+
+    codigo[i++] = 0x55;
+    codigo[i++] = 0x48;
+    codigo[i++] = 0x89;
+    codigo[i++] = 0xe5;  
+
+    //Abre espaco para as 5 variaveis
+    codigo[i++] = 0x48;
+    codigo[i++] = 0x83;
+    codigo[i++] = 0xec;
+    codigo[i++] = 0x20;
+}
+
+funcp gera(FILE *f)
+{
+    funcp cod;
     char c;
+    codigo = (unsigned char*)malloc(300*sizeof(unsigned char));
     long int endereco[MAXLIN];
-    int linha = 1;
+    int linha = 1,i=0;
+    iniciaPilha(codigo);
+
     while ((c = fgetc(f)) != EOF)
     {
         endereco[linha - 1] = (long)(&codigo[i]);
@@ -45,7 +147,7 @@ funcp gera(FILE *f, unsigned char codigo[])
                 }
                 codigo[i++] = 0xC9; // leave
                 codigo[i++] = 0xC3; // ret
-                printf(% d ret % c % d\n, linha, var0, idx0);
+                printf("%d ret %c%d\n", linha, var0, idx0);
                 break;
             }
             case 'v':{
@@ -55,8 +157,8 @@ funcp gera(FILE *f, unsigned char codigo[])
                     error("comando invalido", linha);
                 switch (c0)
                 {
-                    case '<'; {
-                        if (fscanf(f, "%c%d", &var1, &idx1) != 2)
+                    case '<': {
+                        if (fscanf(f, " %c%d", &var1, &idx1) != 2)
                             error("comando invalido", linha);
                         switch (var1) {
                             case '$':{
@@ -94,9 +196,6 @@ funcp gera(FILE *f, unsigned char codigo[])
                                     case 3:
                                         codigo[i++] = 0x55;
                                         break;
-                                    case 3:
-                                        codigo[i++] = 0x55;
-                                        break;
                                 }
                                 addVarLocal(idx0, linha);
                                 break;
@@ -110,10 +209,10 @@ funcp gera(FILE *f, unsigned char codigo[])
                         int idx2;
                         if (fscanf(f, " %c%d %c %c%d", &var1, &idx1, &op, &var2, &idx2) != 5)
                             error("comando invalido", linha);
-                                if (op != = '+' && op != '-' && op != '*')
+                                if (op != '+' && op != '-' && op != '*') // veridica se 'op' ta dentro dos operadores definidos
                                     error("simbolo invalido", linha);
-                        operador(op, var1, var2, idx1, idx2, linha);
-                        printf("%d %c%d = %c%d %c %c%d\n", linha, var0, idx0, var1, idx1, op, va2, idx2);
+                        operador(op, var1, var2,idx0, idx1, idx2, linha);
+                        printf("%d %c%d = %c%d %c %c%d\n", linha, var0, idx0, var1, idx1, op, var2, idx2);
                         break;
                     }
                     default:
@@ -148,4 +247,22 @@ funcp gera(FILE *f, unsigned char codigo[])
     }
     cod = (funcp)codigo;
     return cod;
+}
+
+int main() {
+   // unsigned char codigo[100];
+    funcp funcaoSimples;
+    
+    FILE *f = fopen("simples.txt","r");
+    if (f == NULL) {
+      	printf("Erro na abertura do arquivo\n");
+      	exit(1);
+    }
+    
+    /* traduz a função Simples */
+    funcaoSimples = gera(f);
+    fclose(f);
+    printf("retorno -> %d\n", funcaoSimples(-50));
+    libera(funcaoSimples);
+    return 0;
 }
---
+++ 2023.1/T2/teste.s
@@ -0,0 +1,1 @@
+imull $1, %ebx
